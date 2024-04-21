726666f436ff94ae19de9716cf30827400173613
guilherme.araujo <guilherme.araujo@choiceholding.com>
2024-04-21 17:59:56 -0300

    feat: Criaçao do modulo security group AWS no terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,3 +9,9 @@ module "vpc" {
   public_subnet_cidr = "10.0.1.0/24"
   availability_zone  = "us-east-1a"
 }
+
+module "security_group" {
+  source       = "./modules/security-group"  
+  vpc_id       = module.vpc.vpc_id             
+  projeto_nome = var.projeto_nome
+}
