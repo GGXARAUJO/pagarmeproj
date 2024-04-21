38be563d33c85b4f198819f79b0b57968c86ba50
guilherme.araujo <guilherme.araujo@choiceholding.com>
2024-04-21 17:13:18 -0300

    feat: Criaçao modulo VPC AWS no terraform

---
+++ terraform/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source             = "./modules/vpc"
+  projeto_nome       = var.projeto_nome
+  cidr_block         = "10.0.0.0/16"
+  public_subnet_cidr = "10.0.1.0/24"
+  availability_zone  = "us-east-1a"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "projeto_nome" {
+  description = "Nome do projeto para prefixar nos recursos criados"
+  type        = string
+}
